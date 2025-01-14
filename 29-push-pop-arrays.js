c709d395e3f847781e517d8044a28b82643ad0de
diter14 <diter.terrones14@gmail.com>
2025-01-14 08:55:23 -0500

    session 29. push and pop methods

---
+++ 29-push-pop-arrays.js
@@ -0,0 +1,12 @@
+// 1. push() -> adds a new alement at the end of the array and returns the final lenght
+const fruits = ['apple','banana','orange']
+console.log(fruits)
+const newFruitsLenght = fruits.push('almonds')
+console.log(fruits)
+console.log(newFruitsLenght)
+
+// 2. pop() -> removes the last element of an array and returns it
+console.log(fruits)
+const lastFruitRemoved = fruits.pop()
+console.log(fruits)
+console.log(lastFruitRemoved)
